495cfab8613252bc6143b1405c7ae0ec7726bf19
LyhorngZz <e20220188@dtc.itc.edu.kh>
2025-12-07 17:33:58 +0700

    Initial dis CartPage and CheckoutPage

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!-- src/App.vue -->
 <template>
     <div class="min-h-screen bg-gray-50">
@@ -8,13 +7,3 @@
 
 <script setup lang="ts">
 </script>
-=======
-<script setup>
-
-</script>
-
-<template>
-<div class="text-amber-600">Hello</div>
-<div class="bg-amber-400 text-5xl font-display">Hello World Hi</div>
-</template>
->>>>>>> 2b29b782e69cd48e621f336f0362e602e53a7291
