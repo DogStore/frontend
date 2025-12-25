1fc4588bc622260c7b14c51aba62854c9898e51d
Raby001 <hanraby001@gmail.com>
2025-12-25 19:04:23 +0700

    Take & Fix Authentication part

--- src/pages/admin/DeletethisFile.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <div class="">Hmmbar</div>
-</template>
--- src/router/auth.route.ts
+++ src/router/auth.route.ts
@@ -1,23 +1,14 @@
 // src/router/auth.routes.ts
 import type { RouteRecordRaw } from 'vue-router'
-import LoginView from '@/views/auth/LoginView.vue'
+import Authenication from '@/views/auth/Authentication.vue'
 
 const authRoutes: RouteRecordRaw[] = [
   {
     path: '/login',
     name: 'Login',
-    component: LoginView,
+    component: Authenication,
     meta: { guestOnly: true },
   },
-  // {
-  //   path: '/',
-  //   redirect: () => {
-  //     const role = localStorage.getItem('userRole')
-  //     if (role === 'admin') return { path: '/admin/dashboard' }
-  //     if (role === 'user') return { path: '/user/profile' }
-  //     return { path: '/login' }
-  //   },
-  // },
 ]
 
 export default authRoutes
